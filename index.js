07afb965c3ee43c3f465e75d54e1fbe448a477ea
Brenden Jones <brendenjones4320@gmail.com>
2017-06-26 13:49:02 -0400

    added div

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function updateHeading(ev){
    ev.preventDefault()
-    const f = ev.target
+    const f = event.target
     const name = f.personName.value
     const heading = document.querySelector('h1')
     heading.textContent = name
 }
-const personForm = document.querySelector('#person-form')
+const personForm = document.querySelector("#person-form")
 personForm.addEventListener('submit', updateHeading)
+
+function updateDiv(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const name = f.personName.value
+    const heading = document.querySelector('#stats')
+    heading.textContent = name
+}
+const Div = document.querySelector('#person-form')
+Div.addEventListener('submit', updateDiv)
