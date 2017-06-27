05b8e154cf84c9693ad0c91cc8591c9f7116940c
Brenden Jones <brendenjones4320@gmail.com>
2017-06-27 10:18:49 -0400

    fixed the color part

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+//Class Stuff
 /*function updateHeading(ev){
     ev.preventDefault()
     const f = event.target
@@ -11,38 +11,28 @@ const personForm = document.querySelector("#person-form")
 personForm.addEventListener('submit', updateHeading)*/
 
 
+//
+//
+//
+
+
 //HomeWork
 function updateHeading(ev){
     ev.preventDefault()
     const f = event.target
     const name = f.personName.value
     const age = f.age.value
+    const color = f.color.value
     const stats = document.querySelector('#stats')
     //stats.textContent = name
     //stats.innerHTML = '<p>' + name+ ',' + ' ' + age + '</p>'
     stats.innerHTML = `
     <p> 
-    ${name}, age ${age}
+    ${name}, age ${age}, Fav Color ${color}
     </p>
     `
+    stats.style.color = color
 }
 const personForm = document.querySelector("#person-form")
 personForm.addEventListener('submit', updateHeading)
 
-//
-//
-//
-//Bonus
-
-/*function updateColor(ev){
-    ev.preventDefault()
-    const f =event.target
-    const name = f.color.Value
-    const stats = document.querySelector('#stats p')
-    ArrayList.push(name)
-    stats.textContent = ArrayList
-}
-
-
-const color = document.querySelector('#colorForm')
-color.addEventListener('submit', updateColor)*/
